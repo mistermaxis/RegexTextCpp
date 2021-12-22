6e9dd2e180a14b5f1dfc5c291f75dfb6567614c1
Maximiliano <maximiliano_diaz@gmail.com>
2021-12-22 17:12:56 -0300

    Update main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
+#include <regex>
 
 int main(int, char**) {
-    std::cout << "Hello, no!\n";
+  const char *s = "11-05-2021";
+  std::regex r(R"(\d\d\D\d\d\D\d\d\d\d)");
+  std::cmatch c;
+  std::cout << std::regex_match(s, c, r) << "\n";
+  std::cout << c[0] << "\n";
 }
